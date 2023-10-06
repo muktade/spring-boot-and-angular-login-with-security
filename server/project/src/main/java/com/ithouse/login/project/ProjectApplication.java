99f4033609e784e26ed4242a4b34adbc1e647b1a
softcafecommon <softcafe.common@gmail.com>
2023-10-07 01:43:57 +0600

    ok

--- server/project/src/main/java/com/ithouse/login/project/ProjectApplication.java
+++ server/project/src/main/java/com/ithouse/login/project/ProjectApplication.java
@@ -1,13 +1,13 @@
-package com.ithouse.login;
+package com.ithouse.login.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginApplication {
+public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
+		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
--- server/project/src/test/java/com/ithouse/login/project/ProjectApplicationTests.java
+++ server/project/src/test/java/com/ithouse/login/project/ProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ithouse.login;
+package com.ithouse.login.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginApplicationTests {
+class ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
